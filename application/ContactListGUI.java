505cd699285b695bae63c287fa9a6ba643463245
Sam <sdgoldberg@wisc.edu>
2021-07-16 12:56:46 -0400

    Running Application Screenshots
    Screenshots of JAR application

--- application/ContactListGUI.java
+++ application/ContactListGUI.java
@@ -80,7 +80,7 @@ public class ContactListGUI extends Application {
 		pStage.setScene(fileScene);
 		pStage.show();
 	}
-
+  
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
